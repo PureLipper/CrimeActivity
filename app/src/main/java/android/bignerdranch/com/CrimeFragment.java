ba0a862350f903c72867ac7c718115d05101730c
PureLipper <1090527382@qq.com>
2022-12-06 19:41:02 +0800

    12.6

--- app/src/main/java/android/bignerdranch/com/CrimeFragment.java
+++ app/src/main/java/android/bignerdranch/com/CrimeFragment.java
@@ -1,7 +1,6 @@
 package android.bignerdranch.com;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.*;
